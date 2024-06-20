64ca342a888b6e8f8dfaa5dadeba9d53fff2975e
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-06-20 17:51:27 -0400

    Add Hilt classes and Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,9 +74,9 @@ dependencies {
 
 
     //Retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.11.0")
     //GSON converter
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
