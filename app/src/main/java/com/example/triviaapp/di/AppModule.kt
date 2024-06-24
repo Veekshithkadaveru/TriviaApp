fb62a1fc75cdfacb32421e528e8dbbde603878a4
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-06-24 16:04:58 -0400

    Create Provide Question Repository

--- app/src/main/java/com/example/triviaapp/di/AppModule.kt
+++ app/src/main/java/com/example/triviaapp/di/AppModule.kt
@@ -1,6 +1,7 @@
 package com.example.triviaapp.di
 
 import com.example.triviaapp.network.QuestionApi
+import com.example.triviaapp.repository.QuestionRepository
 import com.example.triviaapp.util.Constants
 import dagger.Module
 import dagger.Provides
@@ -13,6 +14,9 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Singleton
+    @Provides
+    fun provideQuestionRepository(api: QuestionApi)=QuestionRepository(api)
     @Singleton
     @Provides
     fun provideQuestionApi(): QuestionApi {
