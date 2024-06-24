f35153fc2247dd0de310ea1973b6f79affd4c8bb
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-06-24 17:18:14 -0400

    Log Trivia Questions

--- app/src/main/java/com/example/triviaapp/MainActivity.kt
+++ app/src/main/java/com/example/triviaapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.triviaapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,8 +11,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.triviaapp.screens.QuestionsViewModel
 import com.example.triviaapp.ui.theme.TriviaAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,10 +27,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    TriviaHome()
 
                 }
             }
         }
     }
+}
+@Composable
+fun TriviaHome( viewModel: QuestionsViewModel = hiltViewModel())  {
+    Questions(viewModel)
+}
+
+fun Questions(viewModel: QuestionsViewModel) {
+    val questions=viewModel.data.value.data?.toMutableList()
+    if(viewModel.data.value.loading == true)
+        Log.d("Loading","Questions: ...Loading..")
+    else{
+        Log.d("Loading","Questions: Loading Stopped")
+        questions?.forEach{questionItem ->  
+            Log.d("Result","Questions:${questionItem.question}")
+        }
+    }
+
 }
 
