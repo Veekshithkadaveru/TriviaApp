bd88fd6e9a9efbad609f3afc08ad80bb8854067f
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2024-06-26 14:38:40 -0400

    Create Radio Button row and Dotted Line

--- app/src/main/java/com/example/triviaapp/components/Questions.kt
+++ app/src/main/java/com/example/triviaapp/components/Questions.kt
@@ -1,25 +1,37 @@
 package com.example.triviaapp.components
 
 import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathEffect
 import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.triviaapp.model.QuestionItem
 import com.example.triviaapp.screens.QuestionsViewModel
 import com.example.triviaapp.util.AppColors
 
@@ -36,9 +48,18 @@ fun Questions(viewModel: QuestionsViewModel) {
     }
 }
 
-@Preview
+//@Preview
 @Composable
-fun QuestionsDisplay() {
+fun QuestionsDisplay(
+    question: QuestionItem,
+    questionIndex: MutableState<Int>,
+    viewModel: QuestionsViewModel,
+    onNextClicked: (Int) -> Unit
+) {
+    val choicesState = remember(question) {
+        question.choices.toMutableList()
+    }
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), 0f)
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -52,11 +73,66 @@ fun QuestionsDisplay() {
             horizontalAlignment = Alignment.Start
         ) {
             QuestionTracker()
+            DrawDottedLine(pathEffect)
+            Column {
+                Text(
+                    text = "This is a sample paragraph sentence",
+                    modifier = Modifier
+                        .padding(6.dp)
+                        .align(Alignment.Start)
+                        .fillMaxHeight(0.3f),
+                    fontSize = 17.sp,
+                    color = AppColors.mOffWhite,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 22.sp
+                )
+            }
+        }
+        choicesState.forEachIndexed { index, answerText ->
+            Row(
+                modifier = Modifier
+                    .padding(3.dp)
+                    .fillMaxWidth()
+                    .height(45.dp)
+                    .border(
+                        width = 4.dp, brush = Brush.linearGradient(
+                            colors = listOf(AppColors.mOffDarkPurple, AppColors.mOffDarkPurple)
+                        ),
+                        shape = RoundedCornerShape(15.dp)
+                    )
+                    .clip(
+                        RoundedCornerShape(
+                            topStartPercent = 50,
+                            topEndPercent = 50,
+                            bottomEndPercent = 50,
+                            bottomStartPercent = 50
+                        )
+                    )
+                    .background(Color.Transparent)
+            ) {
+
+            }
+
         }
     }
 }
 
-@Preview
+@Composable
+fun DrawDottedLine(pathEffect: PathEffect) {
+    androidx.compose.foundation.Canvas(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(1.dp)
+    ) {
+        drawLine(
+            color = AppColors.mLightGray,
+            start = Offset(0f, 0f),
+            end = Offset(size.width, y = 0f),
+            pathEffect = pathEffect
+        )
+    }
+}
+
 @Composable
 fun QuestionTracker(counter: Int = 10, outOf: Int = 100) {
     Text(
